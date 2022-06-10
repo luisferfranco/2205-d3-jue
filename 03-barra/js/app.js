ef129f6ad3c9642312397b9a36eda7ee952c04dd
Luis F Franco <luisferfranco@gmail.com>
2022-06-09 22:23:00 -0500

    grafica linea completa

--- 03-barra/js/app.js
+++ 03-barra/js/app.js
@@ -1,10 +1,12 @@
+// Selecciones
 const graf = d3.select("#graf")
 
+// Dimensiones
 const anchoTotal = +graf.style("width").slice(0, -2)
 const altoTotal = (anchoTotal * 9) / 16
 
 const margins = {
-  top: 20,
+  top: 60,
   right: 20,
   bottom: 75,
   left: 100,
@@ -12,6 +14,9 @@ const margins = {
 const ancho = anchoTotal - margins.left - margins.right
 const alto = altoTotal - margins.top - margins.bottom
 
+// Escaladores
+
+// Elementos gráficos (layers)
 const svg = graf
   .append("svg")
   .attr("width", anchoTotal)
@@ -33,23 +38,60 @@ const g = svg
   .attr("transform", `translate(${margins.left}, ${margins.top})`)
 
 const load = async () => {
+  // Carga de Datos
   data = await d3.csv("barras.csv", d3.autoType)
-  // data.forEach((d) => {
-  //   d.facturacion = +d.facturacion
-  //   d.clientes = +d.clientes
-  // })
-  console.log(data)
 
+  // Accessor
+  const yAccessor = (d) => d.margen
+  const xAccessor = (d) => d.tienda
+
+  data.sort((a, b) => yAccessor(b) - yAccessor(a))
+
+  // Escaladores
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, yAccessor)])
+    .range([alto, 0])
+
+  console.log(data)
+  console.log(d3.map(data, xAccessor))
+
+  const x = d3
+    .scaleBand()
+    .domain(d3.map(data, xAccessor))
+    .range([0, ancho])
+    .paddingOuter(0.2)
+    .paddingInner(0.1)
+
+  // Rectángulos (Elementos)
   const rect = g
     .selectAll("rect")
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => 75 * i)
-    .attr("y", 0)
-    .attr("width", 50)
-    .attr("height", (d) => d.clientes)
+    .attr("x", (d) => x(xAccessor(d)))
+    .attr("y", (d) => y(yAccessor(d)))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => alto - y(yAccessor(d)))
     .attr("fill", "#e9c46a")
+
+  // Títulos
+  g.append("text")
+    .attr("x", ancho / 2)
+    .attr("y", -15)
+    .classed("titulo", true)
+    .text("Ganancias de las Tiendas")
+
+  // Ejes
+  const xAxis = d3.axisBottom(x)
+  const yAxis = d3.axisLeft(y).ticks(8)
+
+  const xAxisGroup = g
+    .append("g")
+    .attr("transform", `translate(0, ${alto})`)
+    .classed("axis", true)
+    .call(xAxis)
+  const yAxisGroup = g.append("g").classed("axis", true).call(yAxis)
 }
 
 load()
