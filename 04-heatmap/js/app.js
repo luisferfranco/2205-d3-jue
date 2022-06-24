665a456938b66fc32b966d389260dc378e8ed083
Luis F Franco <luisferfranco@gmail.com>
2022-06-23 22:40:39 -0500

    HeatMap final

--- 04-heatmap/js/app.js
+++ 04-heatmap/js/app.js
@@ -1,23 +1,73 @@
-const graf = d3.select("#graf")
+const draw = async (el, col, escala = "linear") => {
+  // Selección
+  const graf = d3.select(el)
 
-const anchoTotal = +graf.style("width").slice(0, -2)
-const altoTotal = anchoTotal * 0.5
+  // Dimensiones
+  const ancho = +graf.style("width").slice(0, -2)
+  const box = (ancho - 10) / col
+  const alto = box * (100 / col) + 10
 
-const margen = { arriba: 50, derecha: 10, abajo: 95, izquierda: 90 }
+  // Area para dibujo
+  const svg = graf
+    .append("svg")
+    .attr("class", "graf")
+    .attr("width", ancho)
+    .attr("height", alto)
 
-const ancho = anchoTotal - margen.izquierda - margen.derecha
-const alto = altoTotal - margen.arriba - margen.abajo
-
-const svg = graf
-  .append("svg")
-  .attr("class", "graf")
-  .attr("width", anchoTotal)
-  .attr("height", altoTotal)
-
-const draw = async (el) => {
+  // Carga de datos
   const dataset = await d3.csv("data.csv", d3.autoType)
+  // dataset.sort((a, b) => a.altura - b.altura)
 
-  console.log(dataset)
+  // Escalador
+  let color
+  switch (escala) {
+    case "linear":
+      color = d3
+        .scaleLinear()
+        .domain(d3.extent(dataset, (d) => d.altura))
+        .range(["#ddddff", "#1111ff"])
+      break
+    case "quantize":
+      color = d3
+        .scaleQuantize()
+        .domain(d3.extent(dataset, (d) => d.altura))
+        .range(["green", "yellow", "red"])
+      break
+    case "threshold":
+      color = d3
+        .scaleThreshold()
+        .domain([1.5, 1.8])
+        .range(["green", "yellow", "red"])
+      break
+  }
+
+  // Dibujo de cuadros
+  svg
+    .append("g")
+    .attr("transform", "translate(5, 5)")
+    .selectAll("rect")
+    .data(dataset)
+    .join("rect")
+    .attr("x", (d, i) => box * (i % col))
+    .attr("y", (d, i) => box * Math.floor(i / col))
+    .attr("width", box - 5)
+    .attr("height", box - 5)
+    .attr("fill", (d) => color(d.altura))
+    .attr("stroke", "#777777")
+
+  svg
+    .append("g")
+    .attr("transform", "translate(5,5)")
+    .selectAll("text")
+    .data(dataset)
+    .join("text")
+    .attr("x", (d, i) => box * (i % col) + box / 2)
+    .attr("y", (d, i) => box * Math.floor(i / col) + box / 2)
+    .text((d) => d.altura)
+    .attr("fill", "black")
+    .attr("text-anchor", "middle")
 }
 
-draw("#hm1")
+draw("#hm2", 10)
+draw("#hm3", 10, "quantize")
+draw("#hm1", 10, "threshold")
